7082c483627e324c7e349c68f21ea16de65ea309
Justin Lee <leejwg.3@gmail.com>
2017-10-07 16:33:57 -1000

    added dojo for play tst code

---
+++ dojo/practiceAsynch.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+console.log('starting app');
+
+setTimeout(() => {
+  console.log('omg ');
+},2000);
+
+setTimeout(() => {
+  console.log('second callback ');
+},0);
+
+console.log('Help');
