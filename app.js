978c893e10f53ce5d150fe6a8175218d698dfc17
Justin Lee <leejwg.3@gmail.com>
2017-10-23 19:13:21 -1000

    added  express backend for JSON data, api request works in postman, err handeling will be done later

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
 // jshint esversion:6
-console.log('Justins Node Weather app!!!! This app allows you to search weather data based on zip Codes~!');
-console.log("use the command line to use the app. Some zip codes you can use are:");
-console.log("HAWAII: 96822 ----  NYC: 10022 ---- Miami: 33128 ---- 94110");
+// console.log('Justins Node Weather app!!!! This app allows you to search weather data based on zip Codes~!');
+// console.log("use the command line to use the app. Some zip codes you can use are:");
+// console.log("HAWAII: 96822 ----  NYC: 10022 ---- Miami: 33128 ---- 94110");
 //libraires used
-const yargs = require('yargs');
+
+// const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
 const weather = require('./weather/weather.js');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
   
+
+function getTheWeather(zipCode, callback){
+
  // the argv.address is the zip code you will enter  
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(results.address);
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-      if (errorMessage) {
-        console.log(errorMessage); 
-      } else {
-        console.log(` overall for today the weather is ${weatherResults.summary}. 
-        The temperature is currently ${weatherResults.temperature} degrees.
-        But It feels more like ${weatherResults.apparentTemperature} degrees.
-        Wind Speeds will be up to ${weatherResults.windSpeed} mph with gusts up to ${weatherResults.windGust} mph`);
-      }
-    });
-  }
-});
+  geocode.geocodeAddress(zipCode, (errorMessage, results) => {
+    if (errorMessage) {
+      console.log(errorMessage);
+    } else {
+      console.log(results.address);
+      weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+        if (errorMessage) {
+          console.log(errorMessage); 
+        } else {
+          callback(null, weatherResults);
+          console.log(` overall for today the weather is ${weatherResults.summary}. 
+          The temperature is currently ${weatherResults.temperature} degrees.
+          But It feels more like ${weatherResults.apparentTemperature} degrees.
+          Wind Speeds will be up to ${weatherResults.windSpeed} mph with gusts up to ${weatherResults.windGust} mph`);
+        }
+      });
+    }
+  });
+}
 
 
+module.exports = getTheWeather;
 
 
 
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 //jshint esversion:6
 
-const express = reqire('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const getTheWeather = require('./app.js'); 
+
 const app = express();
 
 
 
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 
+app.post('/api/jweather', (req, res)=> {
+  console.log('getting the weather ');
 
-
-
-
+  const{ zipcode } = req.body;
+  getTheWeather(zipcode, (err, weatherData) => {
+    console.log(weatherData);
+    res.json(weatherData);
+  });
+});
 
 app.listen(PORT, function() {
   console.log(`Listening for Weather on port: ${PORT}`);
